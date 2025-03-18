9db9158188bd1395b3d344c4993a30d21e4d4039
Dagim <dagimgetaW27@gmail.com>
2025-03-18 21:20:24 +0300

    ternary operator

---
+++ ternary_operator/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int grade = 80;
+
+        String check = (grade >= 70) ? "pass" : "fail";
+
+        System.out.println(check);
+    }
+}
