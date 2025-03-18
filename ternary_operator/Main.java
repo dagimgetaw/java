3791debac5b31fb331770a08ce22ea420250e0e8
Dagim <dagimgetaW27@gmail.com>
2025-03-18 21:24:19 +0300

    ternary operator

--- ternary_operator/Main.java
+++ ternary_operator/Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
         
         int grade = 80;
-
         String check = (grade >= 70) ? "pass" : "fail";
+        System.out.printf("%d %s the test.\n", grade, check);
 
-        System.out.println(check);
+        int num = 3;
+        String oddEven = (num %2 == 0) ? "even" : "odd";
+        System.out.printf("%d is %s.\n", num, oddEven);
+
     }
 }
