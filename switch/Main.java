b3cb78205e467b51422ad4942e22433b32f66f35
Dagim <dagimgetaW27@gmail.com>
2025-03-19 23:01:56 +0300

    switch

---
+++ switch/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        String day = "monday";
+
+        switch (day) {
+            case "monday" -> System.out.print("It is weekday.");
+            case "tuesday" -> System.out.print("It is weekday.");
+            case "wednesday" -> System.out.print("It is weekday.");
+            case "thursday" -> System.out.print("It is weekday.");
+            case "friday" -> System.out.print("It is weekday.");
+            case "saturday" -> System.out.print("It is weekend.");
+            case "sunday" -> System.out.print("It is weekend.");
+            default -> System.out.print(day + " is not in the list.");
+        }
+    }
+}
