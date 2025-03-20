f57a1ef588751649c3c5bc1cd03daa5d2d3d24b4
Dagim <dagimgetaW27@gmail.com>
2025-03-20 21:38:56 +0300

    AND OR NOT operator

--- and_or_not/Main.java
+++ and_or_not/Main.java
@@ -3,8 +3,16 @@ public class Main {
 
         int temp = 20;
 
+        // AND - all statement must be true
         if (temp >= 0 && temp <= 30) {
-            System.err.print("The weather is good");
+            System.err.println("The weather is good");
+        }
+
+        boolean isSunny = false;
+
+        // NOT - if it is false execute if not pass
+        if (!isSunny) {
+            System.err.println("It is cloddy");
         }
     }
 }
