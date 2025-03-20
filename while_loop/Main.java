3fcb123cbb9954767a2d277d8622dacae9a98b99
Dagim <dagimgetaW27@gmail.com>
2025-03-20 22:33:34 +0300

    while loop

--- while_loop/Main.java
+++ while_loop/Main.java
@@ -12,17 +12,30 @@ public class Main {
             name = scanner.nextLine();
         }
 
-        System.out.printf("Hello, %s", name);
+        System.out.printf("Hello, %s\n", name);
 
         String quit = "";
 
         while (!quit.equals("Q")) {
-            System.out.println("\nU are playing the game!");
+            System.out.println("U are playing the game!");
             System.out.print("Press Q to quit: ");
             quit = scanner.nextLine().toUpperCase();
         }
 
-        System.out.print("U quite the game.");
+        System.out.println("U quite the game.");
+
+        int age = 0;
+
+        System.out.print("Enter ur age: ");
+        age = scanner.nextInt();
+
+        while (age < 0) {
+            System.out.println("Ur age can't be negative.");
+            System.out.print("Enter ur age: ");
+            age = scanner.nextInt();
+        }
+
+        System.out.printf("U are %d years old.", age);
 
         scanner.close();
     }
