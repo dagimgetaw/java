c5f24f0e45856d03024044c1cea4903efeaa1cad
Dagim <dagimgetaW27@gmail.com>
2025-03-20 21:23:24 +0300

    AND OR NOT operator

---
+++ and_or_not/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int temp = 20;
+
+        if (temp >= 0 && temp <= 30) {
+            System.err.print("The weather is good");
+        }
+    }
+}
