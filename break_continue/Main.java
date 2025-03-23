997f52102ccb34a7463dba66c90b2207adf45ec0
Dagim <dagimgetaW27@gmail.com>
2025-03-23 14:48:04 +0300

    break & continue keyword usage

---
+++ break_continue/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+            
+            if (i == 3) {
+                continue;
+            }
+
+            if (i == 8) {
+                break;
+            }
+            System.out.print(i+" ");
+        }
+    }
+}
