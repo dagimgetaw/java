b5aacbb17f1e76c5b767fa1a6e1ce5ac641ffd40
Dagim <dagimgetaW27@gmail.com>
2025-03-23 14:19:18 +0300

    for loop

--- for_loop/Main.java
+++ for_loop/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         
-        for (int i =0; i < 6; i++) {
+        for (int i = 0; i < 6; i++) {
             System.out.println("Hi*"+i);
+        }
+
+        for (int i = 0; i <= 10; i += 2) {
+            System.err.println(i);
         }
     }
 }
