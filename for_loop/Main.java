6f55790f15b79f952edeb99bdc17789af9ef1c9d
Dagim <dagimgetaW27@gmail.com>
2025-03-23 14:37:48 +0300

    for loop with scanner

--- for_loop/Main.java
+++ for_loop/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         
@@ -8,5 +10,23 @@ public class Main {
         for (int i = 0; i <= 10; i += 2) {
             System.err.println(i);
         }
+
+        Scanner scanner = new Scanner(System.in);
+
+        int loop;
+        String text;
+
+        System.out.print("Enter how many time u want to loop: ");
+        loop = scanner.nextInt();
+
+        System.out.print("What text u want to be displayed: ");
+        text = scanner.next();
+
+        for (int i = 1; i <= loop; i++) {
+            System.out.printf("%d: %s.\n", i, text);
+        }
+
+        scanner.close();
+
     }
 }
