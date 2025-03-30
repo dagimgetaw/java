0026e866e84f5d0b457d82961e3f628d2075c440
Dagim <dagimgetaW27@gmail.com>
2025-03-30 18:19:40 +0300

    add scanner to the method

--- method/Main.java
+++ method/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        hi();
+
+        String name;
+        int age;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter ur name: ");
+        name = scanner.nextLine();
+
+        System.out.print("Enter ur age: ");
+        age = scanner.nextInt();
+
+        greeting(name, age);
 
     }
-    static void hi() {
-        System.out.println("Hi");
+    static void greeting(String name, int age) {
+        System.out.printf("Welcome, %s\nU are %d old.", name, age);
     }
 }
