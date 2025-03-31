c440fb7dc945edf2cda8d1146bcc27fd5f4baba3
Dagim <dagimgetaW27@gmail.com>
2025-03-31 09:34:10 +0300

    array in java

---
+++ array/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String[] cars = {"Ford","Ferrari","G Wagon", "Bugatti","BMW"};
+
+        System.out.println(cars[0]);
+    }
+}
