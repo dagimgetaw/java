6d38cead302309c84f59ecd6f4ec1bc375ce541b
Dagim <dagimgetaW27@gmail.com>
2025-03-31 09:44:36 +0300

    array in java

--- array/Main.java
+++ array/Main.java
@@ -1,8 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         
         String[] cars = {"Ford","Ferrari","G Wagon", "Bugatti","BMW"};
 
-        System.out.println(cars[0]);
+        System.out.printf("First value is: %s\n", cars[0]);
+        System.out.printf("Total length of the array: %d\n", cars.length);
+        System.out.println("----------------");
+        for (int i = 0; i < cars.length; i++) {
+            System.out.printf("%d: %s\n", i, cars[i]);
+        }
+        System.out.println("----------------");
+        for (String car : cars) {
+            System.out.println(car);
+        }
+        System.out.println("----------------");
+        Arrays.sort(cars);
+        System.out.println("Sorted array");
+        for (String car : cars) {
+            System.out.println(car);
+        }
     }
 }
