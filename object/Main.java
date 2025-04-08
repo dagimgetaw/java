2f27f25e7f838c29f886eabd4a813924c1c58694
Dagim <dagimgetaW27@gmail.com>
2025-04-08 10:01:22 +0300

    read the car class with the methods

--- object/Main.java
+++ object/Main.java
@@ -8,7 +8,20 @@ public class Main {
         System.out.printf("Car color: %s\n", car.color);
         System.out.printf("Car year: %d\n", car.year);
         System.out.printf("Car price: %.2f\n", car.price);
-        System.out.printf("Is the car manual: %s\n", car.isManual);    
+        System.out.printf("Is the car manual: %s\n", car.isManual);  
+        
+        System.out.println(""); 
+
+        System.out.printf("Is the car running: %s\n", car.isRunning);  
+        car.start();
+        System.out.printf("Is the car running: %s\n", car.isRunning);  
+        car.stop();
+        System.out.printf("Is the car running: %s\n", car.isRunning);  
+
+        System.out.println(""); 
+
+        car.drive();
+        car.brake();
 
     }
 }
