fa124b444807654f42e046687a905f9c10c79e88
Dagim <dagimgetaW27@gmail.com>
2025-04-08 10:00:37 +0300

    add method the class

--- object/Car.java
+++ object/Car.java
@@ -1,8 +1,26 @@
 public class Car {
+
     String name = "Ford";
     String model = "Mustang";
     String color = "Black";
     int year = 1996;
     double price = 360000.99;
     boolean isManual = true;
+    boolean isRunning = false;
+
+    void start() {
+        isRunning = true;
+        System.out.println("You start the engine");
+    }
+    void stop() {
+        isRunning = false;
+        System.out.println("You stop the engine");
+    }
+
+    void drive() {
+        System.out.printf("You drive the %s\n", model);
+    }
+    void brake() {
+        System.out.printf("You brake the %s\n", model);
+    }
 }
