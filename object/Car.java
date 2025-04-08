31d94ccbb0925de7c9467ec2a5a5c3b6c1b8174b
Dagim <dagimgetaW27@gmail.com>
2025-04-08 09:48:21 +0300

    car object

---
+++ object/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    String name = "Ford";
+    String model = "Mustang";
+    String color = "Black";
+    int year = 1996;
+    double price = 360000.99;
+    boolean isManual = true;
+}
